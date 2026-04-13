9f0cc359d48487f459a726cc4b18ba52218aedcc
Victor <vitortakayama@gmail.com>
2026-04-13 14:50:30 -0300

    Resolvendo Build do Vite na Vercel

--- plataforma saas/vite.config.js
+++ plataforma saas/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
-import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [
-    basicSsl()
-  ],
   server: {
-    https: true,
+    port: 5173
   }
 });
